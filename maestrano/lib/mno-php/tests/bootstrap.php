6c0a09dc3ea63ef5e7537162ebff84d5e4ccab96
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-01-07 12:06:28 +1100

    added php-saml dependency

--- maestrano/lib/mno-php/tests/bootstrap.php
+++ maestrano/lib/mno-php/tests/bootstrap.php
@@ -2,7 +2,16 @@
 
 define('TEST_ROOT', __DIR__);
 
+// Dependency: php-saml
+define('PHP_SAML_DIR', './../../php-saml/src/OneLogin/Saml/');
+require PHP_SAML_DIR . 'AuthRequest.php';
+require PHP_SAML_DIR . 'Response.php';
+require PHP_SAML_DIR . 'Settings.php';
+require PHP_SAML_DIR . 'XmlSec.php';
+
+// Load tested library: SSO
 define('MNO_PHP_SSO_DIR', './../src/sso/');
 require MNO_PHP_SSO_DIR . 'MnoSsoBaseUser.php';
 
+// Set timezone
 date_default_timezone_set('UTC');
