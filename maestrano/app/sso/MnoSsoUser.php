4519db32c88ef1d2a433e18365b66b4cb5c9ba56
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-01-08 13:55:21 +1100

    added getters

--- maestrano/app/sso/MnoSsoUser.php
+++ maestrano/app/sso/MnoSsoUser.php
@@ -6,5 +6,41 @@
  */
 class MnoSsoUser extends MnoSsoBaseUser
 {
+  /**
+   * Database connection
+   * @var PDO
+   */
+  public $connection = null;
   
+  /**
+   * Get the ID of a local user via Maestrano UID lookup
+   *
+   * @return a user ID if found, null otherwise
+   */
+  protected function _getLocalIdByUid()
+  {
+    $result = $this->connection->query("SELECT ID FROM user WHERE mno_uid = '$this->uid' LIMIT 1")->fetch();
+    
+    if ($result && $result['ID']) {
+      return $result['ID'];
+    }
+    
+    return null;
+  }
+  
+  /**
+   * Get the ID of a local user via email lookup
+   *
+   * @return a user ID if found, null otherwise
+   */
+  protected function _getLocalIdByEmail()
+  {
+    $result = $this->connection->query("SELECT ID FROM user WHERE email = '$this->email' LIMIT 1")->fetch();
+    
+    if ($result && $result['ID']) {
+      return $result['ID'];
+    }
+    
+    return null;
+  }
 }
--- maestrano/app/tests/sso/MnoSsoUserTest.php
+++ maestrano/app/tests/sso/MnoSsoUserTest.php
@@ -1,5 +1,10 @@
 <?php
 
+// Class helper for database connection
+class PDOMock extends PDO {
+    public function __construct() {}
+}
+
 // Class Test
 class MnoSsoUserTest extends PHPUnit_Framework_TestCase
 {
@@ -7,5 +12,113 @@ class MnoSsoUserTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+      $settings = new OneLogin_Saml_Settings;
+      $settings->idpSingleSignOnUrl = 'http://localhost:3000/api/v1/auth/saml';
+
+      // The certificate for the users account in the IdP
+      $settings->idpPublicCertificate = <<<CERTIFICATE
+-----BEGIN CERTIFICATE-----
+MIIDezCCAuSgAwIBAgIJAOehBr+YIrhjMA0GCSqGSIb3DQEBBQUAMIGGMQswCQYD
+VQQGEwJBVTEMMAoGA1UECBMDTlNXMQ8wDQYDVQQHEwZTeWRuZXkxGjAYBgNVBAoT
+EU1hZXN0cmFubyBQdHkgTHRkMRYwFAYDVQQDEw1tYWVzdHJhbm8uY29tMSQwIgYJ
+KoZIhvcNAQkBFhVzdXBwb3J0QG1hZXN0cmFuby5jb20wHhcNMTQwMTA0MDUyMjM5
+WhcNMzMxMjMwMDUyMjM5WjCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEP
+MA0GA1UEBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQG
+A1UEAxMNbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVz
+dHJhbm8uY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVkIqo5t5Paflu
+P2zbSbzxn29n6HxKnTcsubycLBEs0jkTkdG7seF1LPqnXl8jFM9NGPiBFkiaR15I
+5w482IW6mC7s8T2CbZEL3qqQEAzztEPnxQg0twswyIZWNyuHYzf9fw0AnohBhGu2
+28EZWaezzT2F333FOVGSsTn1+u6tFwIDAQABo4HuMIHrMB0GA1UdDgQWBBSvrNxo
+eHDm9nhKnkdpe0lZjYD1GzCBuwYDVR0jBIGzMIGwgBSvrNxoeHDm9nhKnkdpe0lZ
+jYD1G6GBjKSBiTCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEPMA0GA1UE
+BxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQGA1UEAxMN
+bWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVzdHJhbm8u
+Y29tggkA56EGv5giuGMwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCc
+MPgV0CpumKRMulOeZwdpnyLQI/NTr3VVHhDDxxCzcB0zlZ2xyDACGnIG2cQJJxfc
+2GcsFnb0BMw48K6TEhAaV92Q7bt1/TYRvprvhxUNMX2N8PHaYELFG2nWfQ4vqxES
+Rkjkjqy+H7vir/MOF3rlFjiv5twAbDKYHXDT7v1YCg==
+-----END CERTIFICATE-----
+CERTIFICATE;
+
+      // The URL where to the SAML Response/SAML Assertion will be posted
+      $settings->spReturnUrl = 'http://localhost:8888/maestrano/auth/saml/consume.php';
+
+      // Name of this application
+      $settings->spIssuer = 'bla.app.dev.maestrano.io';
+
+      // Tells the IdP to return the email address of the current user
+      $settings->requestedNameIdFormat = 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent';
+      
+      $this->_saml_settings = $settings;
+    }
+    
+    // Used to test protected methods
+    protected static function getMethod($name) {
+      $class = new ReflectionClass('MnoSsoUser');
+      $method = $class->getMethod($name);
+      $method->setAccessible(true);
+      return $method;
+    }
+    
+    public function testFunctionGetLocalIdByUid()
+    {
+      // Specify which protected method get tested
+      $protected_method = self::getMethod('_getLocalIdByUid');
+      
+      // Build User
+      $assertion = file_get_contents(TEST_ROOT . '/support/sso-responses/response_ext_user.xml.base64');
+      $sso_user = new MnoSsoUser(new OneLogin_Saml_Response($this->_saml_settings, $assertion));
+      
+      // Set expected_id
+      $expected_id = 1234;
+      
+      // Create a statement stub
+      $stmt_stub = $this->getMock('PDOStatement');
+      $stmt_stub->expects($this->once())
+                ->method('fetch')
+                ->will($this->returnValue(array("ID" => $expected_id)));
+      
+      // Create a connection stub
+      $pdo_stub = $this->getMock('PDOMock');
+      $pdo_stub->expects($this->once())
+               ->method('query')
+               ->with($this->equalTo("SELECT ID FROM user WHERE mno_uid = '$sso_user->uid' LIMIT 1"))
+               ->will($this->returnValue($stmt_stub));
+               
+      
+      // Test method returns the right id
+      $sso_user->connection = $pdo_stub;
+      $this->assertEquals($expected_id,$protected_method->invokeArgs($sso_user,array()));
+    }
+    
+    public function testFunctionGetLocalIdByEmail()
+    {
+      // Specify which protected method get tested
+      $protected_method = self::getMethod('_getLocalIdByEmail');
+      
+      // Build User
+      $assertion = file_get_contents(TEST_ROOT . '/support/sso-responses/response_ext_user.xml.base64');
+      $sso_user = new MnoSsoUser(new OneLogin_Saml_Response($this->_saml_settings, $assertion));
+      
+      // Set expected_id
+      $expected_id = 1234;
+      
+      // Create a statement stub
+      $stmt_stub = $this->getMock('PDOStatement');
+      $stmt_stub->expects($this->once())
+                ->method('fetch')
+                ->will($this->returnValue(array("ID" => $expected_id)));
+      
+      // Create a connection stub
+      $pdo_stub = $this->getMock('PDOMock');
+      $pdo_stub->expects($this->once())
+               ->method('query')
+               ->with($this->equalTo("SELECT ID FROM user WHERE email = '$sso_user->email' LIMIT 1"))
+               ->will($this->returnValue($stmt_stub));
+               
+      
+      // Test method returns the right id
+      $sso_user->connection = $pdo_stub;
+      $this->assertEquals($expected_id,$protected_method->invokeArgs($sso_user,array()));
     }
 }
