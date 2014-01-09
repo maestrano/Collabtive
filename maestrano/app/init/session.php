294f1113cb91a86a19450c05761427374580fdd5
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-01-09 17:34:09 +1100

    now uses lib_loader

--- maestrano/app/init/session.php
+++ maestrano/app/init/session.php
@@ -4,9 +4,21 @@
 //-----------------------------------------------
 define("MAESTRANO_ROOT", realpath(dirname(__FILE__) . '/../../'));
 
-//------------------------------------------------
-// Load Minimal set of libraries and config files
-//------------------------------------------------
-require MAESTRANO_ROOT . '/lib/mno-php/src/MnoSettings.php';
-require MAESTRANO_ROOT . '/lib/mno-php/src/sso/MnoSsoSession.php';
+//-----------------------------------------------
+// Load Libraries & Settings
+//-----------------------------------------------
+require MAESTRANO_ROOT . '/app/init/_lib_loader.php';
 require MAESTRANO_ROOT . '/app/settings.php'; //set $mno_settings variable
+
+//------------------------------------------------
+// Preparation
+//------------------------------------------------
+// Store accessed url in session
+$mno_session = null;
+if (isset($_SESSION) && $mno_settings->sso_enabled) {
+  $_SESSION['mno_previous_url'] = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+  $mno_session = new MnoSsoSession($mno_settings, $_SESSION);
+}
+
+return $mno_session;
+  
