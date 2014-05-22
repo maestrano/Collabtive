3383c5975330c2645958ac0dc7f1038fdf04fd74
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-05-22 11:07:43 +1000

    refactored config

--- maestrano/lib/mno-php/src/MnoSettings.php
+++ maestrano/lib/mno-php/src/MnoSettings.php
@@ -75,9 +75,10 @@ class MnoSettings
      * @var string
      */
     public function getSsoIdpUrl() {
-      $host = $this->config[$this->environment]['sso_host'];
-      $path = $this->config[$this->environment]['sso_idp_path'];
-      return "${host}${path}";
+      $host = $this->config[$this->environment]['api_host'];
+      $api_base = $this->config[$this->environment]['api_base'];
+      $endpoint = 'auth/saml';
+      return "${host}${api_base}${endpoint}";
     }
     
     /**
@@ -109,9 +110,9 @@ class MnoSettings
      */
     public function getSsoAccessUnauthorizedUrl()
     {
-      $host = $this->config[$this->environment]['sso_host'];
-      $path = $this->config[$this->environment]['sso_access_denied_path'];
-      return "${host}${path}";
+      $host = $this->config[$this->environment]['api_host'];
+      $endpoint = '/app_access_unauthorized';
+      return "${host}${endpoint}";
     }
     
     /**
@@ -121,9 +122,9 @@ class MnoSettings
      */
     public function getSsoAccessLogoutUrl() 
     {
-      $host = $this->config[$this->environment]['sso_host'];
-      $path = $this->config[$this->environment]['sso_access_logout_path'];
-      return "${host}${path}";
+      $host = $this->config[$this->environment]['api_host'];
+      $endpoint = '/app_logout'
+      return "${host}${endpoint}";
     }
     
     /**
@@ -141,9 +142,10 @@ class MnoSettings
      */
     public function getSsoSessionCheckUrl() 
     {
-      $host = $this->config[$this->environment]['sso_host'];
-      $path = $this->config[$this->environment]['sso_session_path'];
-      return "${host}${path}";
+      $host = $this->config[$this->environment]['api_host'];
+      $api_base = $this->config[$this->environment]['api_base'];
+      $endpoint = 'auth/saml';
+      return "${host}${api_base}${endpoint}";
     }
     
     
@@ -185,20 +187,14 @@ class MnoSettings
      */
     private $config = array(
       'test' => array(
-        'sso_host'               => 'http://api-sandbox.maestrano.io',
-        'sso_idp_path'           => '/api/v1/auth/saml',
-        'sso_session_path'       => '/api/v1/auth/saml',
-        'sso_access_denied_path' => '/app_access_unauthorized',
-        'sso_access_logout_path' => '/app_logout',
+        'api_host'               => 'http://api-sandbox.maestrano.io',
+        'api_base'               => '/api/v1/',
         'sso_name_id_format'     => OneLogin_Saml_Settings::NAMEID_PERSISTENT,
         'sso_x509_certificate'   => "-----BEGIN CERTIFICATE-----\nMIIDezCCAuSgAwIBAgIJAOehBr+YIrhjMA0GCSqGSIb3DQEBBQUAMIGGMQswCQYD\nVQQGEwJBVTEMMAoGA1UECBMDTlNXMQ8wDQYDVQQHEwZTeWRuZXkxGjAYBgNVBAoT\nEU1hZXN0cmFubyBQdHkgTHRkMRYwFAYDVQQDEw1tYWVzdHJhbm8uY29tMSQwIgYJ\nKoZIhvcNAQkBFhVzdXBwb3J0QG1hZXN0cmFuby5jb20wHhcNMTQwMTA0MDUyMjM5\nWhcNMzMxMjMwMDUyMjM5WjCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEP\nMA0GA1UEBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQG\nA1UEAxMNbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVz\ndHJhbm8uY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVkIqo5t5Paflu\nP2zbSbzxn29n6HxKnTcsubycLBEs0jkTkdG7seF1LPqnXl8jFM9NGPiBFkiaR15I\n5w482IW6mC7s8T2CbZEL3qqQEAzztEPnxQg0twswyIZWNyuHYzf9fw0AnohBhGu2\n28EZWaezzT2F333FOVGSsTn1+u6tFwIDAQABo4HuMIHrMB0GA1UdDgQWBBSvrNxo\neHDm9nhKnkdpe0lZjYD1GzCBuwYDVR0jBIGzMIGwgBSvrNxoeHDm9nhKnkdpe0lZ\njYD1G6GBjKSBiTCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEPMA0GA1UE\nBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQGA1UEAxMN\nbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVzdHJhbm8u\nY29tggkA56EGv5giuGMwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCc\nMPgV0CpumKRMulOeZwdpnyLQI/NTr3VVHhDDxxCzcB0zlZ2xyDACGnIG2cQJJxfc\n2GcsFnb0BMw48K6TEhAaV92Q7bt1/TYRvprvhxUNMX2N8PHaYELFG2nWfQ4vqxES\nRkjkjqy+H7vir/MOF3rlFjiv5twAbDKYHXDT7v1YCg==\n-----END CERTIFICATE-----"
       ),
       'production' => array(
-        'sso_host'               => 'https://maestrano.com',
-        'sso_idp_path'           => '/api/v1/auth/saml',
-        'sso_session_path'       => '/api/v1/auth/saml',
-        'sso_access_denied_path' => '/app_access_unauthorized',
-        'sso_access_logout_path' => '/app_logout',
+        'api_host'               => 'https://maestrano.com',
+        'api_base'               => '/api/v1/',
         'sso_name_id_format'     => OneLogin_Saml_Settings::NAMEID_PERSISTENT,
         'sso_x509_certificate'   => "-----BEGIN CERTIFICATE-----\nMIIDezCCAuSgAwIBAgIJAPFpcH2rW0pyMA0GCSqGSIb3DQEBBQUAMIGGMQswCQYD\nVQQGEwJBVTEMMAoGA1UECBMDTlNXMQ8wDQYDVQQHEwZTeWRuZXkxGjAYBgNVBAoT\nEU1hZXN0cmFubyBQdHkgTHRkMRYwFAYDVQQDEw1tYWVzdHJhbm8uY29tMSQwIgYJ\nKoZIhvcNAQkBFhVzdXBwb3J0QG1hZXN0cmFuby5jb20wHhcNMTQwMTA0MDUyNDEw\nWhcNMzMxMjMwMDUyNDEwWjCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEP\nMA0GA1UEBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQG\nA1UEAxMNbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVz\ndHJhbm8uY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQD3feNNn2xfEz5/\nQvkBIu2keh9NNhobpre8U4r1qC7h7OeInTldmxGL4cLHw4ZAqKbJVrlFWqNevM5V\nZBkDe4mjuVkK6rYK1ZK7eVk59BicRksVKRmdhXbANk/C5sESUsQv1wLZyrF5Iq8m\na9Oy4oYrIsEF2uHzCouTKM5n+O4DkwIDAQABo4HuMIHrMB0GA1UdDgQWBBSd/X0L\n/Pq+ZkHvItMtLnxMCAMdhjCBuwYDVR0jBIGzMIGwgBSd/X0L/Pq+ZkHvItMtLnxM\nCAMdhqGBjKSBiTCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEPMA0GA1UE\nBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQGA1UEAxMN\nbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVzdHJhbm8u\nY29tggkA8WlwfatbSnIwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQDE\nhe/18oRh8EqIhOl0bPk6BG49AkjhZZezrRJkCFp4dZxaBjwZTddwo8O5KHwkFGdy\nyLiPV326dtvXoKa9RFJvoJiSTQLEn5mO1NzWYnBMLtrDWojOe6Ltvn3x0HVo/iHh\nJShjAn6ZYX43Tjl1YXDd1H9O+7/VgEWAQQ32v8p5lA==\n-----END CERTIFICATE-----"
       )
