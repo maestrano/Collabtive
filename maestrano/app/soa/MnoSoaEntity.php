1b9496cec3959c83d868c719754030e3deb1b9ea
Ahmed Ghonim <ahmed.ghonim@maestrano.com>
2014-06-20 22:57:11 +1000

    SOA Integration

--- maestrano/app/soa/MnoSoaEntity.php
+++ maestrano/app/soa/MnoSoaEntity.php
@@ -9,14 +9,14 @@
 class MnoSoaEntity extends MnoSoaBaseEntity {    
     public function getUpdates($timestamp)
     {
-        MnoSoaLogger::info(__FUNCTION__ .  " start getUpdates (timestamp=" . $timestamp . ")");
+        MnoSoaLogger::info("start timestamp=" . $timestamp);
         $msg = $this->callMaestrano("GET", "updates" . '/' . $timestamp);
         if (empty($msg)) { return false; }
-        MnoSoaLogger::debug(__FUNCTION__ .  " after maestrano call");
+        MnoSoaLogger::debug("after maestrano call");
         if (!empty($msg->projects) && class_exists('MnoSoaProject')) {
-            MnoSoaLogger::debug(__FUNCTION__ .  " has projects");
+            MnoSoaLogger::debug("has projects");
             foreach ($msg->projects as $project) {
-                MnoSoaLogger::debug(__FUNCTION__ .  " project id = " . $project->id);
+                MnoSoaLogger::debug("project id = " . $project->id);
                 try {
                     $mno_org = new MnoSoaProject();
                     $mno_org->receive($project);
@@ -25,7 +25,29 @@ class MnoSoaEntity extends MnoSoaBaseEntity {
             }
         }
         
-        MnoSoaLogger::info(__FUNCTION__ .  " getUpdates successful (timestamp=" . $timestamp . ")");
+        MnoSoaLogger::info("successful (timestamp=" . $timestamp . ")");
+        return true;
+    }
+    
+    public function getProjectUpdates($timestamp)
+    {
+        MnoSoaLogger::info("start timestamp=" . $timestamp);
+        $msg = $this->callMaestrano("GET", "updates" . '/' . $timestamp);
+        if (empty($msg)) { return false; }
+        MnoSoaLogger::debug("after maestrano call");
+        if (!empty($msg->projects) && class_exists('MnoSoaProject')) {
+            MnoSoaLogger::debug("has projects");
+            foreach ($msg->projects as $project) {
+                MnoSoaLogger::debug("project id = " . $project->id);
+                try {
+                    $mno_org = new MnoSoaProject();
+                    $mno_org->receive($project);
+                } catch (Exception $e) {
+                }
+            }
+        }
+        
+        MnoSoaLogger::info("successful (timestamp=" . $timestamp . ")");
         return true;
     }
     
